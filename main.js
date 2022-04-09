f90d70af0f5257dbca407d90b711f141f427bb3c
cssbubble <cssbubble@gmail.com>
2022-04-09 16:42:17 +0100

    image fixes for desktop and page made responsive for 2 medias

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
+// const imgDiv = document.querySelector('.img-div:not(.hidden)')
 const images = document.querySelectorAll('.image');
 const sections = document.querySelectorAll('.section')
+// console.log(imgDiv.length)
 
 left.addEventListener('click', function(){
     change(-1);
@@ -20,18 +22,25 @@ function change(offset) {
         }
     }
     
-
     // check valid change
     if(current + offset < 0 || current + offset >= images.length) return;
-    
+    if(current + offset < 0 || current + offset >= sections.length) return;
 
     images[current].classList.remove("block");
     images[current].classList.add("hidden");
+
+    images[current+3].classList.remove("block");
+    images[current+3].classList.add("hidden");
+
     sections[current].classList.remove("block");
     sections[current].classList.add("hidden");
     
     images[current + offset].classList.remove("hidden");
     images[current + offset].classList.add("block");
+
+    images[current + offset + 3].classList.remove("hidden");
+    images[current + offset + 3].classList.add("block");
+
     sections[current + offset].classList.remove("hidden");
     sections[current + offset].classList.add("block");
 }
